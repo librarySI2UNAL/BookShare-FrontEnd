83b41e52cfbc1eebd78f25c529d345c09160ed05
jorge52an <jorgesolavila@gmail.com>
2017-02-26 00:20:01 -0500

    Style: update style code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class AppComponent {
-  title = 'app works!';
+@Component(
+{
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
+	encapsulation: ViewEncapsulation.None
+} )
+
+export class AppComponent
+{
+	title = "app works!";
 }
