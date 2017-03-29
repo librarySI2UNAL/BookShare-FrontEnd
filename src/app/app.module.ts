26166f7e669277cc01daa5ed842323c6875395e2
Miguel Cuesta <macuestapa@unal.edu.co>
2017-03-28 22:58:02 -0500

    Home page by: Miguel Cuesta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from "./login/app.component";
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule(
 {
 	declarations: [
-		AppComponent
+		AppComponent,
+		HomepageComponent,
+		ContactusComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
-		HttpModule
+		HttpModule,
+		AlertModule.forRoot()
 	],
 	providers: [],
 	bootstrap: [AppComponent]
