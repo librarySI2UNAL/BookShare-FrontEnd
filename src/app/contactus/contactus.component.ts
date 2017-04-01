a3aa636ea07490575e15fb8c387ef408f5599e84
Miguel Cuesta <macuestapa@unal.edu.co>
2017-04-01 18:46:44 -0500

    Página contact us fixed

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
+
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
+
   styleUrls: ['./contactus.component.css']
+
+
+
+
 })
 
 
