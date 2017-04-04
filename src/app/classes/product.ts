df68be8d374dd74133b95166db6ae3ce29b13bbe
jorge52an <jorgesolavila@gmail.com>
2017-04-04 18:44:42 -0500

    Feat: add ProductService class

---
+++ src/app/classes/product.ts
@@ -0,0 +1,12 @@
+import { Book } from "./book";
+import { Collection } from "./collection";
+
+export class Product
+{
+	id: number;
+	description: string;
+	special: boolean;
+	available: boolean;
+	book: Book;
+	collection: Collection;
+}
