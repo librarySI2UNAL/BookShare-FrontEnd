9215d84173982c38b458268a00491575ed91dade
“Edilberto Cañon Pasquel” <ecanonp@unal.edu.co>
2017-04-11 14:24:54 -0500

    Chore: Services-Clases

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@angular/core";
  import { Headers, Http, Response } from "@angular/http";
  import { Observable } from "rxjs/Observable";
- import "rxjs/add/operator/map";
+ import "rxjs/add/operator/toPromise";
  import "rxjs/add/operator/catch";
  
- import { Product } from "../classes/product";
+ import { User } from "../classes/user";
  
  @Injectable()
- export class ProductService
+ export class UserService
  {
  	private headers = new Headers( { "Content-Type": "application/json" } );
  
@@ -32,10 +32,17 @@ import { Injectable } from "@angular/core";
  		return Observable.throw( errMsg );
  	}
  
- 	availableProducts( page: number, perPage: number ): Observable<Product[]>
+ 	login( credentials: any ): Promise<User>
  	{
- 		return this.http.get( `http://localhost:3000/api/v1/products?page=${page}&per_page=${perPage}` )
- 			.map( ( r: Response ) => r.json().products as Product[] )
+ 		return this.http.post( `http://localhost:3000/api/v1/login`, { user: credentials }, { headers: this.headers } ).toPromise()
+ 			.then( response => response.json().data )
+ 			.catch( this.handleError );
+ 	}
+ 
+ 	createUser( user: User ): Promise<User>
+ 	{
+ 		return this.http.post( `http://localhost:3000/api/v1/users`, { user: user }, { headers: this.headers } ).toPromise()
+ 			.then( response => response.json().data )
  			.catch( this.handleError );
  	}
  }
