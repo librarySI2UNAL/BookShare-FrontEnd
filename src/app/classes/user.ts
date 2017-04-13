71c5bb6ac7210eb1d0455f04e8225690835fab34
jorge52an <jorgesolavila@gmail.com>
2017-04-13 16:26:45 -0500

    Feat: update User class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,8 +1,14 @@
+import { City } from "./city";
+
 export class User
- {
- 	id: number;
- 	name: string;
- 	lastName: string;
- 	email: string;
- } 
-  
+{
+	id: number;
+	name: string;
+	lastName: string;
+	email: string;
+	qualification: number;
+	latitude: number;
+	longitude: number;
+	city: City;
+	interests: string[];
+}
