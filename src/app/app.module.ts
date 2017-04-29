204686336f09e613ffd849af7661930d9c8a2642
jorge52an <jorgesolavila@gmail.com>
2017-04-29 16:49:49 -0500

    Chore: general refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
-import { AgmCoreModule } from "angular2-google-maps/core";
+import { AgmCoreModule } from "@agm/core";
 import { FileUploadModule } from "ng2-file-upload";
 
 import { AppComponent } from "./app.component";
