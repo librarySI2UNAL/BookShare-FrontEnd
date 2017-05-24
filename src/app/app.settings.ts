0c3399b5f6a0c051e5a4e7c05c17c51fab8af826
Ivan Rojas <idrojasf@unal.edu.co>
2017-05-24 05:13:57 +0000

    fix:commentaries

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,9 +2,10 @@ import { Headers } from "@angular/http";
 
 export class AppSettings
 {
-	public static SERVER: string = "https://backend-development-debeltranc.c9users.io";
+	//public static SERVER: string = "https://backend-development-debeltranc.c9users.io";
 	//public static SERVER: string = "https://bookshare-backend-migaballengal.c9users.io/";
 	//public static SERVER: string = "http://localhost:3000";
+	public static SERVER: string = "https://back-idrojasf.c9users.io";
 	public static API_ENDPOINT: string = `${AppSettings.SERVER}/api/v1`;
 	public static ACTIVES: any = {
 		home: false,
